e0aafffd8fb18609664dc6c3ac0725a3847d050e
asjadorofejeva <167702650+asjadorofejeva@users.noreply.github.com>
2024-05-28 18:23:42 +0300

    Add files via upload

--- cypress/e2e/e2e-pom/issue-delete-in-pom.cy.js
+++ cypress/e2e/e2e-pom/issue-delete-in-pom.cy.js
@@ -1,25 +1,37 @@
-/**
- * This is an example file and approach for POM in Cypress
- */
 import IssueModal from "../../pages/IssueModal";
 
-describe('Issue delete', () => {
+const issueTitle = "This is an issue of type: Task.";
+
+describe("Issue delete", () => {
   beforeEach(() => {
-    cy.visit('/');
-    cy.url().should('eq', `${Cypress.env('baseUrl')}project/board`).then((url) => {
-    //open issue detail modal with title from line 16  
-    cy.contains(issueTitle).click();
-    });
+    cy.visit("/");
+    cy.url()
+      .should("eq", `${Cypress.env("baseUrl")}project/board`)
+      .then((url) => {
+        cy.contains(issueTitle).click();
+      });
   });
 
-  //issue title, that we are testing with, saved into variable
-  const issueTitle = 'This is an issue of type: Task.';
+  it("Should delete issue successfully", () => {
+    const expectedAmountOfIssuesAfterDeletion = 3;
 
-  it('Should delete issue successfully', () => {
-    //add steps to delete issue
+    IssueModal.clickDeleteButton();
+    IssueModal.confirmDeletion();
+    IssueModal.ensureIssueIsNotVisibleOnBoard(issueTitle);
+    IssueModal.validateAmountOfIssuesInBacklog(
+      expectedAmountOfIssuesAfterDeletion
+    );
   });
 
-  it('Should cancel deletion process successfully', () => {
-    //add steps to start deletion proces but cancel it
+  it("Should cancel delete issue process successfully", () => {
+    const expectedAmountOfIssuesAfterCancel = 4;
+
+    IssueModal.clickDeleteButton();
+    IssueModal.cancelDeletion();
+    IssueModal.closeDetailModal();
+    IssueModal.ensureIssueIsVisibleOnBoard(issueTitle);
+    IssueModal.validateAmountOfIssuesInBacklog(
+      expectedAmountOfIssuesAfterCancel
+    );
   });
 });
